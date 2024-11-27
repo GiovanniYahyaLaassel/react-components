675c9a0bef337bf360336aca6f214a5ec1af58b7
Laassel <lassel.giovanni@gmail.com>
2024-11-27 14:08:22 +0100

    Add header file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
+import React, {useState} from "react";
+import Header from "./components/header";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Header/>
     </>
   )
 }
